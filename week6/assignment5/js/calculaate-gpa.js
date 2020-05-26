7276b43169354ce8fed6f4d836c97fd4c06d927a
Melissa Guzman <melissa.guzman@outlook.com>
2020-05-26 14:44:26 -0700

    Assignment 5

---
+++ week6/assignment5/js/calculaate-gpa.js
@@ -0,0 +1,23 @@
+// Create variables
+var sum = 0;
+var count = 0;
+var scores = 0;
+
+// Creates the average function
+averageScore();
+
+function averageScore(){
+    
+    //create the while loop that adds each score and counts how many times the loop is ran
+    while(scores !== 999) {
+        //use parseInt and prompt to display the prompt
+        scores = parseInt(prompt("Enter test score\n" + "Or enter 999 to end entries\n")); 
+
+        sum += scores;  
+
+        count++;   
+        
+    }
+}
+//account for the addition of 999 to end the while loop; 
+var average = (sum - 999)/(count - 1); 
